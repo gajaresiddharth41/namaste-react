6a4eb72d0d0d99908d40c7c95f8a54ed76d8fe1c
siddharth <gajaresiddharth41@gmail.com>
2024-04-23 08:21:06 +0530

    episode 1

---
+++ App.js
@@ -0,0 +1,14 @@
+
+
+
+const parent  =React.createElement("div2",{id:"parent"},"parent")
+
+const heading = React.createElement("h1", {}, "Hello world from react ");
+
+console.log(heading)
+
+const parent = React.createElement('h2',{},"from h1")
+
+console.log(parent)
+
+
